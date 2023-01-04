b67dc1a8984103a372582b8ae2891aa28deb374d
ishyexe <ishrar12345@gmail.com>
2023-01-03 20:12:30 -0600

    added open source link

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App"> weather app </div>;
+  return (
+    <div className="App">
+      <h1>weather app </h1>
+      <footer>
+        This project was created by ishy ♡ and is
+        <a
+          href="https://github.com/ishyexe/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open sourced on github
+        </a>
+      </footer>
+    </div>
+  );
 }
